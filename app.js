091e1e9d9d8853f2e2755683e308dd5c54d04740
Ahmed Gemechu <Ahmedgemechu14@gmail.com>
2024-02-01 11:44:41 +0300

    add the new apo

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 
-app.get('/', (res, req) => {
-  res.statusCode(200).send('hello from the server end');
+// app.get('/', (req, res) => {
+//   res
+//     .status(200)
+//     .json({ message: 'hello from the server end', app: 'Netours' });
+// });
+
+// app.post('/', (req, res) => {
+//   res.send('You can post to this input');
+// });
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tours,
+    },
+  });
 });
 
 const port = 3000;
