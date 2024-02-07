11c13083cce8a057a9fb2d649392a62a4b55da8b
Ahmed Gemechu <Ahmedgemechu14@gmail.com>
2024-02-07 18:03:38 +0300

    asd

--- app.js
+++ app.js
@@ -7,19 +7,25 @@ const tourRouter = require('./Routes/tourRoutes');
 const app = express();
 
 //1)Middlewares
-
-app.use(morgan('dev'));
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
-app.use((req, res, next) => {
-  req.requestTime = new Date().toDateString();
-  next();
-});
+// app.use((req, res, next) => {
+//   req.requestTim = new Date().toDateString();
+//   res.status(200).json({
+//     status: 'success',
+//     message: 'Server is up and running',
+//   });
+//   next();
+// });
 ///////////////////
 
+//3)Route
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
-
-//4) START SERVER
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,6 +1,45 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const app = require('./app');
+dotenv.config({ path: './config.env' });
 
-const port = 3000;
+const DB = process.env.DATABASE;
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('Database connected'));
+
+const tourSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Name is required'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'Price is required'],
+  },
+});
+const Tour = mongoose.model('Tour', tourSchema);
+
+const tourTest = new Tour({
+  name: 'The Park',
+  price: 499,
+});
+
+tourTest
+  .save()
+  .then((doc) => console.log(`Succefully created: ${doc}`))
+  .catch((err) => console.log(`ERROR 💥${err}`));
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`App is runinng in port ${port}...`);
+  console.log(`App is running on port ${port}...`);
 });
