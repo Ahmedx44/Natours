f84f68bc5928f6ddc11a998a0c31e7dae01e0724
Ahmed Gemechu <Ahmedgemechu14@gmail.com>
2024-02-16 10:40:41 +0300

    hello

--- Controllers/authControllers.js
+++ Controllers/authControllers.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken');
 const catchAsync = require('../utils/catchAsync');
 const User = require('./../model/userModel');
+const AppError = require('./../utils/appError');
 
 exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create({
@@ -19,5 +20,20 @@ exports.signup = catchAsync(async (req, res, next) => {
     data: {
       User: newUser,
     },
+  });
+});
+
+exports.login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    next(new AppError('Please provide email and password', 400));
+  }
+
+  //3)If everything is ok,send token to client
+  const token = 'hello';
+  res.status(200).json({
+    status: 'success',
+    token,
   });
 });
